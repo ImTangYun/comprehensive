58a7a7d90c82b9bf93553d0349225a3a3beaf20d
lenovo <candy@ubuntu.ubuntu-domain>
2014-06-04 22:02:07 +0800

    modify some format of this code

--- C++/algorithm/string/qh_string.cpp
+++ C++/algorithm/string/qh_string.cpp
@@ -1,3 +1,5 @@
+// yuntang
+// file:qh_string.cpp
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,283 +10,233 @@ using namespace std;
 namespace qh
 {
     // 
-	string::string() {
+string::string() {
+	data_ = safe_alloc(15);
+	cap = 15;
+	data_[0] = '\0';
+	len_ = 0;
+}
+
+string::string(const char* s) {
+	int length = strlen(s);
+	if(s == NULL || length == 0) {
 		data_ = safe_alloc(15);
 		cap = 15;
 		data_[0] = '\0';
 		len_ = 0;
+		return;
 	}
-
-	string::string(const char* s) {
- 		int length = strlen(s);
- 		if(s == NULL || length == 0) {
-			data_ = safe_alloc(15);
-			cap = 15;
-			data_[0] = '\0';
-			len_ = 0;
-			return;
+	size_t i;
+	if(length > 7) {
+		cap = 2 * length;
+		data_ = safe_alloc(2 * length);
+		for(i = 0; i < length; ++i) {
+			data_[i] = s[i];
 		}
-		size_t i;
-		if(length > 7) {
-			cap = 2 * length;
-			data_ = safe_alloc(2 * length);
-			for(i = 0; i < length; ++i) {
-				data_[i] = s[i];
-			}
-			data_[i] = '\0';
-			len_ = length;
-		}else {
-			data_ = safe_alloc(15);
-			cap = 15;
-			len_ = length;
-			for(i = 0; i < length; ++i) {
-				data_[i] = s[i];
-			}
-			data_[i] = '\0';
+		data_[i] = '\0';
+		len_ = length;
+	}else {
+		data_ = safe_alloc(15);
+		cap = 15;
+		len_ = length;
+		for(i = 0; i < length; ++i) {
+			data_[i] = s[i];
 		}
+		data_[i] = '\0';
 	}
+}
 
-	string::string(const char* s, size_t len) {
-		if(s == NULL || len == 0)	{
-			data_ = safe_alloc(15);
-			data_[0] = '\0';
-			cap = 15;
-			len_ = 0;
-			return;
-		}
-		size_t i;
-		if(len > 7) {
-			cap = 2 * len;
-			data_ = safe_alloc(2 * len);
-			for(i = 0; i < len; ++i) {
-				data_[i] = s[i];
-			}
-			data_[i] = '\0';
-			len_ = len;
-		}else {
- 			data_ = safe_alloc(15);
-			cap = 15;
-			len_ = len;
-			for(i = 0; i < len; ++i) {
-				data_[i] = s[i];
-			}
-			data_[i] = '\0';
-		}
+string::string(const char* s, size_t len) {
+	if(s == NULL || len == 0)	{
+		data_ = safe_alloc(15);
+		data_[0] = '\0';
+		cap = 15;
+		len_ = 0;
+		return;
 	}
-
-	string::string(const string& rhs) {
-		len_ = rhs.size();
-		size_t i = 0;
-		if(len_ <= 7) {
-			data_ = safe_alloc(15);
-			cap = 15;
-			while(i < len_) {
-				data_[i] = rhs.data_[i];
-				++i;
-			}
-			data_[i] = '\0';
-		}else{
-			data_ = safe_alloc(2 * len_);
-			cap = 2 * len_;
-			while(i < len_) {
-				data_[i] = rhs.data_[i];
-				++i;
-			}
-			data_[i] = '\0';
+	size_t i;
+	if(len > 7) {
+		cap = 2 * len;
+		data_ = safe_alloc(2 * len);
+		for(i = 0; i < len; ++i) {
+			data_[i] = s[i];
 		}
+		data_[i] = '\0';
+		len_ = len;
+	}else {
+		data_ = safe_alloc(15);
+		cap = 15;
+		len_ = len;
+		for(i = 0; i < len; ++i) {
+			data_[i] = s[i];
+		}
+		data_[i] = '\0';
 	}
+}
 
-	string& string::operator=(const string& rhs) {
-		int len_ = rhs.size();
-		size_t i = 0;
-		if(data_ != NULL) {
-			delete [] data_;
+string::string(const string& rhs) {
+	len_ = rhs.size();
+	size_t i = 0;
+	if(len_ <= 7) {
+		data_ = safe_alloc(15);
+		cap = 15;
+		while(i < len_) {
+			data_[i] = rhs.data_[i];
+			++i;
 		}
-		if(len_ <= 7) {
-			data_ = safe_alloc(15);
-			cap = 15;
-			while(i < len_) {
-				data_[i] = rhs.data_[i];
-				++i;
-			}
-			data_[i] = '\0';
-		}else{
-			data_ = safe_alloc(2 * len_);
-			cap = 2 * len_;
-			while(i < len_) {
-				data_[i] = rhs.data_[i];
-				++i;
-			}
-			data_[i] = '\0';
+		data_[i] = '\0';
+	}else{
+		data_ = safe_alloc(2 * len_);
+		cap = 2 * len_;
+		while(i < len_) {
+			data_[i] = rhs.data_[i];
+			++i;
 		}
+		data_[i] = '\0';
+	}
+}
+
+string& string::operator=(const string& rhs) {
+	int len_ = rhs.size();
+	size_t i = 0;
+	if(data_ != NULL) {
+		delete [] data_;
+	}
+	if(len_ <= 7) {
+		data_ = safe_alloc(15);
+		cap = 15;
+		while(i < len_) {
+			data_[i] = rhs.data_[i];
+			++i;
+		}
+		data_[i] = '\0';
+	}else{
+		data_ = safe_alloc(2 * len_);
+		cap = 2 * len_;
+		while(i < len_) {
+			data_[i] = rhs.data_[i];
+			++i;
+		}
+		data_[i] = '\0';
+	}
+	return *this;
+}
+
+string& string::operator=(const char* rhs) {
+	int length = strlen(rhs);
+	if(rhs == NULL || length == 0)	{
+		data_ = safe_alloc(15);
+		data_[0] = '\0';
+		cap = 15;
+		len_ = 0;
 		return *this;
 	}
-    
-	string& string::operator=(const char* rhs) {
- 		int length = strlen(rhs);
- 		if(rhs == NULL || length == 0)	{
-			data_ = safe_alloc(15);
-			data_[0] = '\0';
-			cap = 15;
-			len_ = 0;
-			return *this;
+	size_t i;
+	if(length > 7) {
+		cap = 2 * length;
+		data_ = safe_alloc(2 * length);
+		for(i = 0; i < length; ++i) {
+			data_[i] = rhs[i];
 		}
+		data_[i] = '\0';
+		len_ = length;
+	}else {
+		data_ = safe_alloc(15);
+		cap = 15;
+		len_ = length;
+		for(i = 0; i < length; ++i) {
+			data_[i] = rhs[i];
+		}
+		data_[i] = '\0';
+	}
+	return *this;
+}
+//dtor
+string::~string(){
+	if(cap > 0) delete [] data_;
+}
+
+//get
+size_t string::size() const {
+	return len_;
+}
+const char * string::data() const {
+	return data_;
+}
+const char* string::c_str() const {
+	if(size() == 0) {
+		return "";
+	}
+	return data_;
+}
+size_t string::capacity() const {
+	return cap;
+}
+// set & get
+
+char &string::operator[](size_t index) {
+	if(index >= len_) {
+		printf("下标越界了！\n\n");//throw exception("越界了！");
+		char *p = "";
+		return *p;
+	}
+	return data_[index];
+}
+string& string::operator+(const string& rhs) {
+	string *temp = new string;
+	temp->len_ = size() + rhs.size();
+	size_t i = 0;
+	if(temp->len_ <= 7) {
+		temp->data_ = safe_alloc(15);//new char[15];
+		temp->cap = 15;
+		while(i < size()) {
+			temp->data_[i] = data_[i];
+			++i;
+		}
+		while(i < temp->len_) {
+			temp->data_[i] = rhs.data_[i - size()];
+			++i;
+		}
+		temp->data_[i] = '\0';
+	}else{
+		temp->data_ = safe_alloc(2 * len_);//new char[2 * len_];
+		temp->cap = 2 * temp->len_;
+		while(i < size()) {
+			temp->data_[i] = data_[i];
+			++i;
+		}
+		while(i < temp->len_) {
+			temp->data_[i] = rhs.data_[i - size()];
+			++i;
+		}
+		temp->data_[i] = '\0';
+	}
+	return *temp;
+}
+
+void string::reserve(int c) {
+	if(c > cap) {
+		cap = c;
+		char *p = data_;
+		data_ = safe_alloc(cap);//new char[cap];
 		size_t i;
-		if(length > 7) {
-			cap = 2 * length;
-			data_ = safe_alloc(2 * length);
-			for(i = 0; i < length; ++i) {
-				data_[i] = rhs[i];
-			}
-			data_[i] = '\0';
-			len_ = length;
-		}else {
-			data_ = safe_alloc(15);
-			cap = 15;
-			len_ = length;
-			for(i = 0; i < length; ++i) {
-				data_[i] = rhs[i];
-			}
-			data_[i] = '\0';
+		for(i = 0; i < size(); ++i) {
+			data_[i] = p[i];
 		}
-		return *this;
+		data_[i] = '\0';
+		if(p != NULL) delete [] p;
 	}
-    //dtor
-	string::~string(){
-		if(cap > 0) delete [] data_;
+}
+
+char* string::safe_alloc(size_t len) {
+	char* temp;
+	try {
+		temp = new char[len];
 	}
-
-    //get
-	size_t string::size() const {
-		return len_;
+	catch(bad_alloc x) {
+		cerr << "Allocation failure! \n";
+		return NULL;
 	}
-	const char * string::data() const {
-		return data_;
-	}
-	const char* string::c_str() const {
-		if(size() == 0) {
-			return "";
-		}
-		return data_;
-	}
-	size_t string::capacity() const {
-		return cap;
-	}
-    // set & get
-
-	char &string::operator[](size_t index) {
-		if(index >= len_) {
-			printf("下标越界了！\n\n");//throw exception("越界了！");
-#ifdef WIN32
-			system("pause");
-#endif	
-			char *p = "";
-			return *p;
-		}
-		return data_[index];
-	}
-	string& string::operator+(const string& rhs) {
-		string *temp = new string;
-		temp->len_ = size() + rhs.size();
-		size_t i = 0;
-		if(temp->len_ <= 7) {
-			temp->data_ = safe_alloc(15);//new char[15];
-			temp->cap = 15;
-			while(i < size()) {
-				temp->data_[i] = data_[i];
-				++i;
-			}
-			while(i < temp->len_) {
-				temp->data_[i] = rhs.data_[i - size()];
-				++i;
-			}
-			temp->data_[i] = '\0';
-		}else{
-			temp->data_ = safe_alloc(2 * len_);//new char[2 * len_];
-			temp->cap = 2 * temp->len_;
-			while(i < size()) {
-				temp->data_[i] = data_[i];
-				++i;
-			}
-			while(i < temp->len_) {
-				temp->data_[i] = rhs.data_[i - size()];
-				++i;
-			}
-			temp->data_[i] = '\0';
-		}
-		return *temp;
-	}
-
-	void string::reserve(int c) {
-		if(c > cap) {
-			cap = c;
-			char *p = data_;
-			data_ = safe_alloc(cap);//new char[cap];
-			size_t i;
-			for(i = 0; i < size(); ++i) {
-				data_[i] = p[i];
-			}
-			data_[i] = '\0';
-			if(p != NULL) delete [] p;
-		}
-	}
-
-	char* string::safe_alloc(size_t len) {
-		char* temp;
-		try {
-			temp = new char[len];
-		}
-		catch(bad_alloc x) {
-			cerr << "Allocation failure! \n";
-			return NULL;
-		}
-		return temp;
-	}
-    /*string::string()
-        : data_(NULL), len_(0)
-    {
-    }
-
-    string::string( const char* s )
-    {
-    }
-
-    string::string( const char* s, size_t len )
-    {
-    }
-
-    string::string( const string& rhs )
-    {
-
-    }
-
-    string& string::operator=( const string& rhs )
-    {
-        return *this;
-    }
-
-    string::~string()
-    {
-
-    }
-
-    size_t string::size() const
-    {
-        return len_;
-    }
-
-    const char* string::data() const
-    {
-        return NULL;
-    }
-
-    const char* string::c_str() const
-    {
-        return NULL;
-    }
-
-    char* string::operator[]( size_t index )
-    {
-        return NULL;
-    }*/
+	return temp;
+}
 }
--- C++/algorithm/string/qh_string.h
+++ C++/algorithm/string/qh_string.h
@@ -1,3 +1,5 @@
+// yuntang
+// file qh_string.h
 #ifndef QIHOO_STRING_H_
 #define QIHOO_STRING_H_
 
@@ -5,64 +7,38 @@
 
 namespace qh
 {
-    class string {
-    public:
-        //ctor
-		//时间复杂度O（15）
-		//空间复杂度O（15）
-        string();
+class string {
+public:
+	string();
 
- 		//时间复杂度O（strlen（s））
-		//空间复杂度O（strlen（s））
-		string(const char* s);
+	string(const char* s);
 
- 		//时间复杂度O（strlen（len））
-		//空间复杂度O（strlen（len））
-        string(const char* s, size_t len);
+	string(const char* s, size_t len);
 
-  		//时间复杂度O（rhs.size()）
-		//空间复杂度O（rhs.size()）
-        string(const string& rhs);
+	string(const string& rhs);
 
-  		//时间复杂度O（rhs.size()）
-		//空间复杂度O（rhs.size()）
-        string& operator=(const string& rhs);
+	string& operator=(const string& rhs);
 
- 		//时间复杂度O（strlen（rhs））
-		//空间复杂度O（strlen（rhs））
-        string& operator=(const char* rhs);
- 		//时间复杂度O（rhs.size() + size()）
-		//空间复杂度O（rhs.size() + size()）
-		string& operator+(const string& rhs);
+	string& operator=(const char* rhs);
+	
+	string& operator+(const string& rhs);
+	
+	~string();
 
-        //dtor
-        ~string();
+	//get
+	size_t size() const;
+	const char* data() const;
+	const char* c_str() const;
+	size_t capacity() const;
+	char &operator[](size_t index);
+	void reserve(int c);
+private:
+	char*  data_;
+	size_t len_;
+	size_t cap;
 
-        //get
- 		//时间复杂度O（1）
-        size_t size() const;
- 		//时间复杂度O（1）
-        const char* data() const;
- 		//时间复杂度O（1）
-        const char* c_str() const;
- 		//时间复杂度O（1）
-		size_t capacity() const;
-
-        // set & get
- 		//时间复杂度O（1）
-        char &operator[](size_t index);
-
- 		//最好时间复杂度O（1），最差时间复杂度O(size())
-		void reserve(int c);
-    private:
-        char*  data_;
-        size_t len_;
-		size_t cap;
-
-  		//时间复杂度O（len）
-		//空间复杂度O（len）
-		char* safe_alloc(size_t len);
-    };
+	char* safe_alloc(size_t len);
+};
 }
 
 #endif
