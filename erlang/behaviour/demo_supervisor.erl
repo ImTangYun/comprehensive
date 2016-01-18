1420a41a05d9ed36162d2c501a66fffeb4a7bfd0
tangyun <1282823258@qq.com>
2016-01-18 15:22:52 +0800

    .

--- erlang/behaviour/demo_supervisor.erl
+++ erlang/behaviour/demo_supervisor.erl
@@ -11,6 +11,6 @@ start_link() ->
     supervisor:start_link({local,?MODULE}, ?MODULE, []).
 
 init([]) ->
-    {ok, {{simple_one_for_one, 1, 60},
+    {ok, {{one_for_one, 1, 60},
         [{id, {demo_supervisor_call, start_link, [hello, ok]},
                 permanent, 5000, worker, [demo_supervisor_call]}]}}.
